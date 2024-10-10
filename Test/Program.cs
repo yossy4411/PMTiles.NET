a474255810ca6b91f42bc158106b8adc36701885
yossy4411 <yossy4411.dev@gmail.com>
2024-10-10 22:27:02 +0900

    Add test project

---
+++ Test/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+var pm = new PMTiles.NET.PMTiles();
+
+if (await pm.CreateFromUrl("https://cyberjapandata.gsi.go.jp/xyz/optimal_bvmap-v1/optimal_bvmap-v1.pmtiles"))
+{
+
+    var header = await pm.GetHeader();
+
+    Console.WriteLine(header.SpecVersion);
+}
+else
+{
+    Console.WriteLine("Failed to create PMTiles");
+}
