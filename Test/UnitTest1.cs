8f18be03f1a1abb9a72a3294fa929f9b5a23f8d0
yossy4411 <yossy4411.dev@gmail.com>
2024-10-18 12:09:23 +0900

    Add test to parse vector tiles

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,7 +1,11 @@
+using Mapbox.Vector.Tile;
 using PMTiles;
 
 namespace Test;
 
+/// <summary>
+/// Tests for reading PMTiles
+/// </summary>
 public class Tests
 {
     private PMTilesReader? _reader;
@@ -13,7 +17,7 @@ public class Tests
     }
 
     [Test]
-    public async Task Test1()
+    public async Task HeaderTest()
     {
         Assert.That(_reader, Is.Not.Null);
         var header = await _reader.GetHeader();
@@ -22,10 +26,62 @@ public class Tests
             Assert.That(header.SpecVersion, Is.EqualTo(3)); // PMTiles Spec Version 3
             Assert.That(header.TileType, Is.EqualTo(TileType.Mvt)); // Mapbox Vector Tiles
         });
-
+    }
+    
+    /// <summary>
+    /// Test for reading a tile, at Tokyo, zoom level 9
+    /// </summary>
+    [Test]
+    public async Task TileTest1()
+    {
+        Assert.That(_reader, Is.Not.Null);
+        var tileId = PMTilesHelper.ZxyToTileId(9, 454, 201);  // at Tokyo, Tile ID is 286674
+        Assert.That(tileId, Is.EqualTo(286674));
+        
         var tile = await _reader.GetTileZxy(9, 454, 201);  // Tokyo
         Assert.That(tile, Is.Not.Null);
-        Assert.That(tile!, Is.Not.Empty);
-        Console.WriteLine($"Tile size: {tile!.Length} bytes");
+
+        var layers = VectorTileParser.Parse(tile);  // Mapbox.Vector.Tile
+        Assert.That(layers, Is.Not.Null);
+        Assert.That(layers, Is.Not.Empty);
+        Assert.Multiple(() =>
+        {
+            Assert.That(layers.Any(layer => layer.Name == "AdmBdry")); // layer name "AdmBdry" exists (Administrative Boundary 行政区域の境界)
+            Assert.That(layers.Any(layer => layer.Name == "RailCL")); // layer name "RailCL" exists (Railway Center Line 鉄道中心線)
+            Assert.That(layers.Any(layer => layer.Name == "RdCL")); // layer name "RdCL" exists (Road Center Line 道路中心線)
+            Assert.That(layers.Any(layer => layer.Name == "WA")); // layer name "WA" exists (Water Area 水域)
+        });
     }
+
+    /// <summary>
+    /// Test for reading a tile, at zoom level 14 (max)
+    /// </summary>
+    [Test]
+    public async ValueTask TileTest2()
+    {
+        Assert.That(_reader, Is.Not.Null);
+        var tileId = PMTilesHelper.ZxyToTileId(14, 14552, 6451);  // at Tokyo, Tile ID is 293539700
+        Assert.That(tileId, Is.EqualTo(293554628));
+        
+        var tile = await _reader.GetTileZxy(14, 14552, 6451);  // Tokyo
+        Assert.That(tile, Is.Not.Null);
+        
+        var layers = VectorTileParser.Parse(tile);  // Mapbox.Vector.Tile
+        Assert.That(layers, Is.Not.Null);
+        Assert.That(layers, Is.Not.Empty);
+        Assert.Multiple(() =>
+        {
+            Assert.That(layers.Any(layer => layer.Name == "AdmBdry"));  // layer name "AdmBdry" exists (Administrative Boundary 行政区域の境界)
+            Assert.That(layers.Any(layer => layer.Name == "AdmArea"));  // layer name "AdmArea" exists (Administrative Area 行政区域)
+            Assert.That(layers.Any(layer => layer.Name == "RailCL"));   // layer name "RailCL" exists (Railway Center Line 鉄道中心線)
+            Assert.That(layers.Any(layer => layer.Name == "RdCL"));     // layer name "RdCL" exists (Road Center Line 道路中心線)
+            Assert.That(layers.Any(layer => layer.Name == "WA"));       // layer name "WA" exists (Water Area 水域)
+            Assert.That(layers.Any(layer => layer.Name == "BldA"));     // layer name "BldA" exists (Building area 建物の塗り)
+            Assert.That(layers.Any(layer => layer.Name == "Anno"));     // layer name "Anno" exists (Annotation 注記)
+        });
+        // todo: Add more tests
+    }
+    
+    // todo: Add more tests and implements e.g. Writing PMTiles, etc.
+    
 }
